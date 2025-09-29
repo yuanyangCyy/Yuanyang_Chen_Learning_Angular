263fa94ad0ef6ea5f1a23c132a48dae974ebe214
yuanyangCyy <YC43@myscc.ca>
2025-09-29 14:41:25 -0400

    change jason version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app';
+import { AppComponent } from './app.component';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -18,6 +18,8 @@ describe('App', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, yuanyang-chen-learning-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, yuanyang-chen-learning-angular'
+    );
   });
 });
---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import type { Movie } from './Shared/Models/movie';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title: string = 'yuanyang-chen-learning-angular';
+
+  movie1: Movie = { id: 1, title: 'The Battle at Lake Changjin', director: 'Chen Kaige', year: 2021, inTheaters: false };
+  movie2: Movie = { id: 2, title: 'Inception',                 director: 'Christopher Nolan', year: 2010, inTheaters: false };
+  movie3: Movie = { id: 3, title: 'Parasite',                  director: 'Bong Joon-ho',      year: 2019, inTheaters: false };
+  movie4: Movie = { id: 4, title: 'Avatar: The Way of Water',  director: 'James Cameron',     year: 2022, inTheaters: true };
+  movie5: Movie = { id: 5, title: 'Interstellar',              director: 'Christopher Nolan', year: 2014, inTheaters: false };
+
+  movieList: Movie[] = [this.movie1, this.movie2, this.movie3, this.movie4, this.movie5];
+}
