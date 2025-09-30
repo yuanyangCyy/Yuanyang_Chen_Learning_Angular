140fb921a1c28c8cda0b42f33514dd5fdcabf3da
yuanyangCyy <YC43@myscc.ca>
2025-09-29 21:11:05 -0400

    sept 5-8 done

--- src/app/movie-list/movie-list.ts
+++ src/app/movie-list/movie-list.ts
@@ -1,17 +1,20 @@
-import {Component, Input} from '@angular/core';
-import {Movie} from '../Shared/Models/movie';
-import {MovieListItem} from '../movie-list-item/movie-list-item';
+import { Component } from '@angular/core';
+import { Movie } from '../Shared/Models/movie';
+import { MovieListItem } from '../movie-list-item/movie-list-item';
 
 @Component({
   selector: 'app-movie-list',
-  imports: [
-    MovieListItem
-  ],
+  standalone: true,
+  imports: [MovieListItem],
   templateUrl: './movie-list.html',
-  styleUrl: './movie-list.css'
+  styleUrls: ['./movie-list.css']
 })
 export class MovieList {
-  @Input() movie!:Movie;
-  movies: any;
-
+  movies: Movie[] = [
+    { id: 1, title: 'The Battle at Lake Changjin', director: 'Chen Kaige', year: 2021, inTheaters: false },
+    { id: 2, title: 'Inception', director: 'Christopher Nolan', year: 2010, inTheaters: false },
+    { id: 3, title: 'Parasite', director: 'Bong Joon-ho', year: 2019, inTheaters: false },
+    { id: 4, title: 'Avatar: The Way of Water', director: 'James Cameron', year: 2022, inTheaters: true },
+    { id: 5, title: 'Interstellar', director: 'Christopher Nolan', year: 2014, inTheaters: false }
+  ];
 }
