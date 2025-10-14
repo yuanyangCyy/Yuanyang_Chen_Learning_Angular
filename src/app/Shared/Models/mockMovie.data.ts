4d86c7d6135b21338aa351c31c270745546a2664
yuanyangCyy <YC43@myscc.ca>
2025-10-14 10:49:54 -0400

    done as5

--- src/app/Shared/Models/mockMovie.data.ts
+++ src/app/Shared/Models/mockMovie.data.ts
@@ -1,9 +1,45 @@
 import { Movie } from './movie';
 
+
 export const movieList: Movie[] = [
-  { id: 1, title: 'The Battle at Lake Changjin', director: 'Chen Kaige', year: 2021, inTheaters: false },
-  { id: 2, title: 'Inception', director: 'Christopher Nolan', year: 2010, inTheaters: false },
-  { id: 3, title: 'Parasite', director: 'Bong Joon-ho', year: 2019, inTheaters: false },
-  { id: 4, title: 'Avatar: The Way of Water', director: 'James Cameron', year: 2022, inTheaters: true },
-  { id: 5, title: 'Interstellar', director: 'Christopher Nolan', year: 2014, inTheaters: false }
+  {
+    id: 1,
+    title: 'The Battle at Lake Changjin',
+    director: 'Chen Kaige',
+    year: 2021,
+    inTheaters: false,
+    image: 'assets/images/lakechangjin.jpeg'
+  },
+  {
+    id: 2,
+    title: 'Inception',
+    director: 'Christopher Nolan',
+    year: 2010,
+    inTheaters: false,
+    image: 'assets/images/inception.jpeg'
+  },
+  {
+    id: 3,
+    title: 'Parasite',
+    director: 'Bong Joon-ho',
+    year: 2019,
+    inTheaters: false,
+    image: 'assets/images/parasite.jpeg'
+  },
+  {
+    id: 4,
+    title: 'Avatar: The Way of Water',
+    director: 'James Cameron',
+    year: 2022,
+    inTheaters: true,
+    image: 'assets/images/avatar2.jpeg'
+  },
+  {
+    id: 5,
+    title: 'Interstellar',
+    director: 'Christopher Nolan',
+    year: 2014,
+    inTheaters: false,
+    image: 'assets/images/interstellar.jpeg'
+  }
 ];
--- src/app/Shared/Models/movie.ts
+++ src/app/Shared/Models/movie.ts
@@ -5,4 +5,5 @@ export interface Movie{
   year: number;
   rating?: number;
   inTheaters: boolean;
+  image: string;
 }
