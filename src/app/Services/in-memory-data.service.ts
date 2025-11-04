9e4e2369ac1683f99925c9648583f659fdac6234
yuanyangCyy <YC43@myscc.ca>
2025-11-03 22:37:12 -0500

    as7 step 6 done

---
+++ src/app/Services/in-memory-data.service.ts
@@ -0,0 +1,52 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Movie } from '../Shared/Models/movie';
+
+export class InMemoryDataService implements InMemoryDbService {
+  // returns an object with a movies property,
+  // which is an array of Movie objects
+  createDb(): { movies: Movie[] } {
+    /*
+
+    */
+    const movies: Movie[] = [
+      {
+        id: 1,
+        title: 'Inception',
+        year: 2010,
+        director: 'Christopher Nolan',
+        rating: 9.0,
+        inTheaters: false,
+        image: 'assets/images/1.jpg'
+      },
+      {
+        id: 2,
+        title: 'Avatar: The Way of Water',
+        year: 2022,
+        director: 'James Cameron',
+        rating: 8.5,
+        inTheaters: true,
+        image: 'assets/images/2.jpg'
+      },
+      {
+        id: 3,
+        title: 'Interstellar',
+        year: 2014,
+        director: 'Christopher Nolan',
+        rating: 8.6,
+        inTheaters: false,
+        image: 'assets/images/3.jpg'
+      },
+      {
+        id: 4,
+        title: 'Oppenheimer',
+        year: 2023,
+        director: 'Christopher Nolan',
+        rating: 8.7,
+        inTheaters: true,
+        image: 'assets/images/4.jpg'
+      }
+    ];
+
+    return { movies };
+  }
+}
